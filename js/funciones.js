1975147642776ab5549dd26c4cc017ee71d8af3e
SergioCadavid97 <sergiocadavid9@gmail.com>
2020-07-15 20:41:25 -0500

    Actualizacion del codigo, incorporando Java Script

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,22 @@
 function abrirAlerta() {
     alert("Hola esto es una alerta!!");
 }
+
+function login(){
+    var usuario= document.getElementById('textUser');
+    var contraseña= document.getElementById('txtPass');
+
+    console.log(textUser);
+
+    if (usuario.nodeValue=="admin" && contraseña.nodeValue=="admin") {
+     location.href= "pagina2.";
+    } else {
+        location.href="pagina1.html";
+    }
+}
+function hacerAlgo(){
+    var reloj = document.getElementById('reloj');
+    reloj.innerHTML =new Date(); 
+}
+ //hacerAlgo();
+ setInterval(hacerAlgo, 1000);
